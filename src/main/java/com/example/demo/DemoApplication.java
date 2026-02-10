bd7b9475f4c88f9ea52c98c0b2b44780a793e2f0
sputnik413-beep <tsitstudy@gmail.com>
2026-02-10 10:13:11 +0900

    自動ビルド着火テストのためコメント追加

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        //コメントだけ挿入してGitへPushし、自動ビルドの仕組みを着火
     }
 
 }
